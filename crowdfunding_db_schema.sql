10dc590ef12e7b9cccc8fc9fb328a4616a517c31
Mark Helotie <43053988+mark-helotie@users.noreply.github.com>
2023-09-06 00:12:51 -0400

    Adding SQL work

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Project 2 -- Crowdfunding_ETL
+
+-- Uncomment these to DROP existing tables
+-- DROP TABLE campaign;
+-- DROP TABLE contacts;
+-- DROP TABLE category;
+-- DROP TABLE subcategory;
+
+-- Create the four tables
+CREATE TABLE IF NOT EXISTS category (
+    category_id VARCHAR(10)   NOT NULL,
+    category VARCHAR(20)   NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (category_id)
+);
+
+CREATE TABLE IF NOT EXISTS subcategory (
+    subcategory_id VARCHAR(10)   NOT NULL,
+    subcategory VARCHAR(20)   NOT NULL,
+    CONSTRAINT pk_subcategory PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE IF NOT EXISTS campaign (
+    cf_id INTEGER   NOT NULL,
+    contact_id INTEGER   NOT NULL,
+    company_name VARCHAR(100)   NOT NULL,
+    description VARCHAR(100)   NOT NULL,
+    goal NUMERIC   NOT NULL,
+    pledged NUMERIC   NOT NULL,
+    outcome VARCHAR(20)   NOT NULL,
+    backers_count INTEGER   NOT NULL,
+    country VARCHAR(20)   NOT NULL,
+    currency VARCHAR(20)   NOT NULL,
+    launch_date DATE   NOT NULL,
+    end_date DATE   NOT NULL,
+    category_id VARCHAR(10)   NOT NULL,
+    subcategory_id VARCHAR(10)   NOT NULL,
+    CONSTRAINT pk_campaign PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+    contact_id INTEGER   NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    email VARCHAR(50)   NOT NULL,
+    CONSTRAINT pk_contacts PRIMARY KEY (contact_id)
+);
+
+-- Construct FK constraints
+ALTER TABLE campaign 
+    ADD CONSTRAINT fk_campaign_category_id FOREIGN KEY(category_id)
+    REFERENCES category (category_id);
+
+ALTER TABLE campaign 
+    ADD CONSTRAINT fk_campaign_subcategory_id FOREIGN KEY(subcategory_id)
+    REFERENCES subcategory (subcategory_id);
+
+ALTER TABLE contacts 
+    ADD CONSTRAINT fk_contacts_contact_id FOREIGN KEY(contact_id)
+    REFERENCES campaign (contact_id);
